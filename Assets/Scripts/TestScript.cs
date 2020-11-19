c42ebd1913d35f1c8f7c7199cb1e69e8cb587982
TerenceHQZ <63692900+TerenceHQZ@users.noreply.github.com>
2020-11-19 09:47:35 +0800

    1.0.0
    Initial

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameManager.GetPlayerMoney() <= 0)
+        {
+            StartCoroutine(ReduceHappiness());
+        }
+    }
+
+    IEnumerator ReduceHappiness()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        GameManager.SetHappiness(-1);
+
+        Debug.Log(GameManager.GetPlayerHappiness());
+
+        if (GameManager.GetPlayerHappiness() <= 0)
+        {
+            PlayerPrefs.SetInt("AccountExist", 0);
+            Debug.Log("Lose");
+            yield return null;
+        }
+        else
+        {
+            yield return StartCoroutine(ReduceHappiness());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
