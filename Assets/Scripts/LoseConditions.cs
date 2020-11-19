5aee766733b63a821bd94cf759714a8c5c65596a
ponggos <64051399+ponggos@users.noreply.github.com>
2020-11-19 09:55:41 +0800

    Add files via upload

---
+++ Assets/Scripts/LoseConditions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseConditions : MonoBehaviour
+{
+    void Update()
+    {
+        Lose();
+    }
+
+    void Lose()
+    {
+        // get playermoney and happiness variable from other script 
+
+        if(playerMoney <= 0 && playerHappiness <= 0)
+        {
+            // show lose screen
+            // end the game
+        }
+    }
+}
