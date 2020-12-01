41ca77bf68394cc80469e6ee3bd86c907f5c9d65
karlelijah <74691709+karlelijah@users.noreply.github.com>
2020-12-01 12:03:11 +0800

    Shop

---
+++ Assets/Scripts/Buy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Buy : MonoBehaviour
+{
+    public void buy(int price)
+    {
+        GameManager.SetMoney(-price);
+    }
+}
