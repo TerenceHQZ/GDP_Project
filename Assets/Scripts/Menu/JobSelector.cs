9dab0fd0a412a1e495dbc40ee3f865f4da94c649
TerenceHQZ <63692900+TerenceHQZ@users.noreply.github.com>
2020-12-05 22:25:26 +0800

    Job Selector UI Script

---
+++ Assets/Scripts/Menu/JobSelector.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class JobSelector : MonoBehaviour
+{
+    public void WarehouseJobSelect()
+    {
+        JobManager.SetJob(1);
+    }
+
+    public void DeliveryJobSelect()
+    {
+        JobManager.SetJob(2);
+    }
+
+    public void FoodBeverageJobSelect()
+    {
+        JobManager.SetJob(3);
+    }
+
+    public void RetailJobSelect()
+    {
+        JobManager.SetJob(4);
+    }
+}
