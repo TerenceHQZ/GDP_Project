b6c3e301c92438e94127808b9443d05de77219cd
karlelijah <74691709+karlelijah@users.noreply.github.com>
2020-12-06 19:25:08 +0800

    Updated

---
+++ Assets/Scripts/Menu/LoseMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseMenu : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
