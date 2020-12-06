02ae31d906d8802cadc9693c7fc8d8357dea8170
TerenceHQZ <63692900+TerenceHQZ@users.noreply.github.com>
2020-12-06 18:26:24 +0800

    Open Job Window

---
+++ Assets/Scripts/Menu/OpenJobButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class OpenJobButton : MonoBehaviour
+{
+    public GameObject character;
+    public GameObject JobWindow;
+
+    private void Update()
+    {
+        // Debug.Log(JobManager.GetJob());
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name == "Office")
+                {
+                    Vector3 buildingPos = hit.transform.position;
+                    Vector3 characterPos = character.transform.position;
+
+                    Debug.Log((buildingPos - characterPos).magnitude);
+
+                    if ((buildingPos - characterPos).magnitude <= 2.5f)
+                    {
+                        JobWindow.SetActive(true);
+                    }
+                }
+            }
+        }
+    }
+}
