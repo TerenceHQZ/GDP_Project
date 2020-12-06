5d6dbd8016b39928cd30c44545368df89eaacdb0
TerenceHQZ <63692900+TerenceHQZ@users.noreply.github.com>
2020-12-06 17:06:10 +0800

    Update Shop Button

---
+++ Assets/Scripts/Menu/UpdateShopButton.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class UpdateShopButton : MonoBehaviour
+{
+    public GameObject[] BuyButton;
+    public GameObject[] ClaimedButton;
+
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("OwnedFan") == 1)
+        {
+            BuyButton[0].SetActive(false);
+            ClaimedButton[0].SetActive(true);
+        }
+
+        if (PlayerPrefs.GetInt("OwnedTable") == 1)
+        {
+            BuyButton[1].SetActive(false);
+            ClaimedButton[1].SetActive(true);
+        }
+
+        if (PlayerPrefs.GetInt("OwnedChair") == 1)
+        {
+            BuyButton[2].SetActive(false);
+            ClaimedButton[2].SetActive(true);
+        }
+
+        if (PlayerPrefs.GetInt("OwnedBed") == 1)
+        {
+            BuyButton[3].SetActive(false);
+            ClaimedButton[3].SetActive(true);
+        }
+
+        if (PlayerPrefs.GetInt("OwnedPC") == 1)
+        {
+            BuyButton[4].SetActive(false);
+            ClaimedButton[4].SetActive(true);
+        }
+    }
+}
