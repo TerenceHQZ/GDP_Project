93f00fb61c5a06d32179a724c772b2fc91966257
TerenceHQZ <63692900+TerenceHQZ@users.noreply.github.com>
2020-12-06 18:09:03 +0800

    Update JobSelector.cs

--- Assets/Scripts/Menu/JobSelector.cs
+++ Assets/Scripts/Menu/JobSelector.cs
@@ -6,40 +6,56 @@ public class JobSelector : MonoBehaviour
     public void WarehouseJobSelect()
     {
         if(JobManager.GetJob() != 0)
+        {
             JobManager.SetJob(1);
+            Debug.Log("Warehouse selected");
+        }
         else
         {
             // ALREADY HAVE A JOB
+            Debug.Log("Already have a job");
         }
     }
 
     public void DeliveryJobSelect()
     {
         if (JobManager.GetJob() != 0)
+        {
             JobManager.SetJob(2);
+            Debug.Log("Delivery selected");
+        }
         else
         {
             // ALREADY HAVE A JOB
+            Debug.Log("Already have a job");
         }
     }
 
     public void FoodBeverageJobSelect()
     {
         if (JobManager.GetJob() != 0)
+        {
             JobManager.SetJob(3);
+            Debug.Log("Delivery selected");
+        }
         else
         {
             // ALREADY HAVE A JOB
+            Debug.Log("Already have a job");
         }
     }
 
     public void RetailJobSelect()
     {
         if (JobManager.GetJob() != 0)
+        {
             JobManager.SetJob(4);
+            Debug.Log("Delivery selected");
+        }
         else
         {
             // ALREADY HAVE A JOB
+            Debug.Log("Already have a job");
         }
     }
 }
