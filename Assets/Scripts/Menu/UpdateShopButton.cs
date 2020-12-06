c46ed9658af14c2a68df7acb7135dbdd9912f0c4
TerenceHQZ <63692900+TerenceHQZ@users.noreply.github.com>
2020-12-06 17:35:29 +0800

    Reset Furniture

--- Assets/Scripts/Menu/UpdateShopButton.cs
+++ Assets/Scripts/Menu/UpdateShopButton.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class UpdateShopButton : MonoBehaviour
 {
@@ -36,5 +37,44 @@ public class UpdateShopButton : MonoBehaviour
             BuyButton[4].SetActive(false);
             ClaimedButton[4].SetActive(true);
         }
+
+        StartCoroutine(SetButton());
+    }
+
+    IEnumerator SetButton()
+    {
+        yield return new WaitForSeconds(3f);
+
+        if (PlayerPrefs.GetInt("OwnedFan") == 1)
+        {
+            BuyButton[0].SetActive(false);
+            ClaimedButton[0].SetActive(true);
+        }
+
+        if (PlayerPrefs.GetInt("OwnedTable") == 1)
+        {
+            BuyButton[1].SetActive(false);
+            ClaimedButton[1].SetActive(true);
+        }
+
+        if (PlayerPrefs.GetInt("OwnedChair") == 1)
+        {
+            BuyButton[2].SetActive(false);
+            ClaimedButton[2].SetActive(true);
+        }
+
+        if (PlayerPrefs.GetInt("OwnedBed") == 1)
+        {
+            BuyButton[3].SetActive(false);
+            ClaimedButton[3].SetActive(true);
+        }
+
+        if (PlayerPrefs.GetInt("OwnedPC") == 1)
+        {
+            BuyButton[4].SetActive(false);
+            ClaimedButton[4].SetActive(true);
+        }
+
+        yield return StartCoroutine(SetButton());
     }
 }
