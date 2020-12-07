918cc62331a6410ea857e9d59d9c6caeb5af80f7
ponggos <guchitsuki@gmail.com>
2020-12-07 17:26:48 +0800

    change

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,6 +5,7 @@ public class CharacterMovement : MonoBehaviour
     public Joystick playerJoystick;
     public float movementSpeed;
     private Rigidbody rb;
+    public Vector3 clampPos;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +18,11 @@ public class CharacterMovement : MonoBehaviour
         Vector3 cameraDirection = new Vector3(playerJoystick.Horizontal, 0f, playerJoystick.Vertical);
         cameraDirection = Camera.main.transform.TransformDirection(cameraDirection);
         cameraDirection.y = 0.0f;
-
+        
         rb.AddForce(cameraDirection * movementSpeed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+
+        clampPos.x = Mathf.Clamp(transform.position.x, -12, 12);
+        clampPos.z = Mathf.Clamp(transform.position.z, -12, 12);
+        transform.position = clampPos;
     }
 }
--- Assets/Scripts/School.cs
+++ Assets/Scripts/School.cs
@@ -5,11 +5,26 @@ public class School : MonoBehaviour
 {
     public GameObject character;
     public GameObject schoolFade;
+    public GameObject floatingSprite;
     public static int loseHappinessAmount = 20;
     public static bool wentToSchool = false;
 
     void Update()
     {
+        if (!wentToSchool)
+        {
+            floatingSprite.SetActive(true);
+            floatingSprite.transform.position = new Vector3(floatingSprite.transform.position.x,
+            floatingSprite.transform.position.y + (Mathf.Sin(Time.time) * 0.001f),
+            floatingSprite.transform.position.z);
+
+            floatingSprite.transform.Rotate(0, 0.25f, 0);
+        }
+        else
+        {
+            floatingSprite.SetActive(false);
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
@@ -26,6 +41,7 @@ public class School : MonoBehaviour
                     {
                         if ((DayTimeManager.GetHour() <= 8 && DayTimeManager.GetMinute() <= 59) && !wentToSchool)
                         {
+                            
                             StartCoroutine(SchoolFade());
                             StartCoroutine(GoToSchool());
                         }
@@ -35,6 +51,7 @@ public class School : MonoBehaviour
 
                             GameManager.SetHappiness(GameManager.GetHappiness() - 10);
 
+                            floatingSprite.SetActive(false);
                             StartCoroutine(SchoolFade());
                             StartCoroutine(GoToSchool());
                         }
